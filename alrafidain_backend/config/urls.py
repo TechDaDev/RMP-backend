229a94a11c5000d8d6414988942088c7656e41ca
TechDaDev <techda.dev@gmail.com>
2026-05-15 20:05:47 +0300

    Serve media files in debug URL config

--- alrafidain_backend/config/urls.py
+++ alrafidain_backend/config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -24,3 +26,6 @@ urlpatterns = [
     path("api/knowledge-base/", include("apps.knowledge_base.urls")),
     path("api/rag/", include("apps.rag.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
